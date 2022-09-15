7da822f8e339e1c054d5c2a4ec1ba1303d9fcf61
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-09-15 19:40:46 -0300

    campos fillable y relacion con values

--- app/Models/Ad.php
+++ app/Models/Ad.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Ad extends Model
 {
     use HasFactory; 
+
+    protected $fillable = [
+        'link',
+        'clicks_hired',
+        'location'
+    ];
+
+    public function values() {
+        return $this->belongsToMany(Value::class)->withTimestamps();
+    }
 }
