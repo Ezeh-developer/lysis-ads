243c94da5cbebf48c95dc9d5aaaed8ee260e9ca5
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-09-15 22:51:04 -0300

    metodo para pedir campo link

--- app/Models/Ad.php
+++ app/Models/Ad.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Ad extends Model
 {
@@ -17,5 +18,26 @@ class Ad extends Model
 
     public function values() {
         return $this->belongsToMany(Value::class)->withTimestamps();
+    }
+
+    public function getLinkByValues($values) {
+        DB::table('ads')
+            ->select('link',)
+            ->join('ad_value', 'ads.id', '=', 'ad_value.ad_id')
+            ->join('values', 'ad_value.value_id', '=', 'values.id')
+            ->join('tags', 'values.tag_id', '=', 'tags.id')
+            ->where([
+                ['tags.name', 'like', $values["firstCondition"]["tag"]],
+                ['value', 'like', $values["firstCondition"]["value"]]
+            ])
+            ->orWhere([
+                ['tags.name', 'like', $values["secondCondition"]["tag"]],
+                ['value', 'like', $values["secondCondition"]["value"]]
+            ])
+            ->orWhere([
+                ['tags.name', 'like', $values["thirdCondition"]["tag"]],
+                ['value', 'like', $values["thirdCondition"]["value"]]
+            ])
+            ->get();
     }
 }
