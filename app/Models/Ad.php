d80b2c816cb6f66f4ea2d45423e36520a504223f
Facundo Odera <facundoodera011@gmail.com>
2022-11-14 22:54:53 -0300

    funcion getLinkByValues en Ad model

--- app/Models/Ad.php
+++ app/Models/Ad.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\Ads\Value;
+use Illuminate\Support\Facades\DB;
 
 class Ad extends Model
 {
-    use HasFactory; 
+    use HasFactory,SoftDeletes; 
 
     protected $fillable = [
         'image',
@@ -21,5 +22,30 @@ class Ad extends Model
 
     public function values() {
         return $this->belongsToMany(Value::class)->withTimestamps();
+    }
+
+    public function getLinkByValues($values) {
+        return DB::table('ads')
+            ->join('ad_value', 'ads.id', '=', 'ad_value.ad_id')
+            ->join('values', 'ad_value.value_id', '=', 'values.id')
+            ->join('tags', 'values.tag_id', '=', 'tags.id')
+            ->where([
+                ['tags.name', '=', $values["firstCondition"]["tag"]],
+                ['values.value', '=', $values["firstCondition"]["value"]],
+                ['ads.location', '=', $values["fourthCondition"]["location"]]
+            ])
+            ->orWhere([
+                ['tags.name', '=', $values["secondCondition"]["tag"]],
+                ['values.value', '=', $values["secondCondition"]["value"]],
+                ['ads.location', '=', $values["fourthCondition"]["location"]]
+            ])
+            ->orWhere([
+                ['tags.name', '=', $values["thirdCondition"]["tag"]],
+                ['values.value', '=', $values["thirdCondition"]["value"]],
+                ['ads.location', '=', $values["fourthCondition"]["location"]]
+            ])
+            ->select('link')
+            ->inRandomOrder()
+            ->get();
     }
 }
